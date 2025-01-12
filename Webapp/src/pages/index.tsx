d8b8e3ab1092d9d62bf71f65cab09b6dc14f5c90
Sudipta Dey <sdptd20@gmail.com>
2025-01-13 03:54:32 +0530

    Update index.tsx

--- Webapp/src/pages/index.tsx
+++ Webapp/src/pages/index.tsx
@@ -1,3 +1,13 @@
+import GameEmbed from "@/components/GameEmbed";
+import HighScoresTable from "@/components/HighScoresTable";
+
 export default function Home() {
-    return <>Test</>;
+    return (
+        <>
+            <div className="flex flex-col items-center">
+                <GameEmbed />
+                <HighScoresTable />
+            </div>
+        </>
+    );
 }
