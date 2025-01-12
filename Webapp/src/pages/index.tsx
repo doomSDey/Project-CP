b8a9d876e81a05d4cf06e659d05b2d97e9833a37
Sudipta Dey <sdptd20@gmail.com>
2025-01-13 04:08:06 +0530

    make the highscore in a retractable panel

--- Webapp/src/pages/index.tsx
+++ Webapp/src/pages/index.tsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import GameEmbed from "@/components/GameEmbed";
 import HighScoresTable from "@/components/HighScoresTable";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Import Bootstrap Icons CSS
 
 export default function Home() {
-    return (
-        <div className="flex flex-col lg:flex-row h-screen w-screen bg-gray-100 overflow-hidden">
-            {/* Game Section */}
-            <div className="lg:w-3/5 w-full h-2/5 lg:h-full bg-black shadow-lg m-4 rounded-lg overflow-hidden">
-                <GameEmbed />
-            </div>
+  const [isPanelVisible, setIsPanelVisible] = useState(false);
 
-            {/* High Scores Section */}
-            <div className="lg:w-2/5 w-full h-auto bg-white shadow-lg rounded-lg m-4 p-6 overflow-y-auto">
-                <HighScoresTable />
-            </div>
-        </div>
-    );
+  const togglePanel = () => {
+    setIsPanelVisible(!isPanelVisible);
+  };
+
+  return (
+    <div className="flex h-screen w-screen bg-gray-100 overflow-hidden relative">
+      {/* Game Section */}
+      <div
+        className={`transition-all duration-300 ${
+          isPanelVisible ? "w-3/5" : "w-full"
+        } bg-black shadow-lg m-4 rounded-lg overflow-hidden`}
+      >
+        <GameEmbed />
+      </div>
+
+      {/* Toggle Icon */}
+      <div
+        className="fixed top-10 transform -translate-y-1/2 right-0 bg-blue-500 text-white p-3 rounded-l-lg cursor-pointer z-50"
+        onClick={togglePanel}
+      >
+        <i className="bi bi-trophy text-2xl"></i>
+      </div>
+
+      {/* High Scores Panel */}
+      <div
+        className={`fixed top-0 right-0 h-full bg-white shadow-lg transition-transform transform ${
+          isPanelVisible ? "translate-x-0" : "translate-x-full"
+        } w-2/5 p-6 overflow-y-auto z-40`}
+      >
+        <h2 className="text-xl font-semibold mb-4">High Scores</h2>
+        <HighScoresTable />
+      </div>
+    </div>
+  );
 }
