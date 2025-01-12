981f21d3875399c71e698146a4b19b2032ff9a0a
Sudipta Dey <sdptd20@gmail.com>
2025-01-13 04:02:25 +0530

    improve UI

--- Webapp/src/components/GameEmbed.tsx
+++ Webapp/src/components/GameEmbed.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const GameEmbed: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-black">
-      <iframe
-        src="https://project-capybara.s3.ap-south-1.amazonaws.com/index.html"
-        title="Unity Game"
-        className="w-full h-full"
-        allowFullScreen
-      ></iframe>
+    <div className="flex justify-center items-center bg-black w-full h-full">
+      <div className="relative w-full max-w-[1920px] h-0" style={{ paddingTop: '56.25%' }}>
+        <iframe
+          src="https://project-capybara.s3.ap-south-1.amazonaws.com/index.html"
+          title="Unity Game"
+          className="absolute top-0 left-0 w-full h-full"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
--- Webapp/src/pages/index.tsx
+++ Webapp/src/pages/index.tsx
@@ -3,11 +3,16 @@ import HighScoresTable from "@/components/HighScoresTable";
 
 export default function Home() {
     return (
-        <>
-            <div className="flex flex-col items-center">
+        <div className="flex flex-col lg:flex-row h-screen w-screen bg-gray-100 overflow-hidden">
+            {/* Game Section */}
+            <div className="lg:w-3/5 w-full h-2/5 lg:h-full bg-black shadow-lg m-4 rounded-lg overflow-hidden">
                 <GameEmbed />
+            </div>
+
+            {/* High Scores Section */}
+            <div className="lg:w-2/5 w-full h-auto bg-white shadow-lg rounded-lg m-4 p-6 overflow-y-auto">
                 <HighScoresTable />
             </div>
-        </>
+        </div>
     );
 }
